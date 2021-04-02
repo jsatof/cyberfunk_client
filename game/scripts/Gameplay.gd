aad530d4308c17bab2fcf75295a4d884d0ba69fe
jsferrarelli <jsferr4@gmail.com>
2021-04-02 08:38:39 -0400

    Improve button input detection - add check for action_is_released() event

--- game/scripts/Gameplay.gd
+++ game/scripts/Gameplay.gd
@@ -1,26 +1,6 @@
 extends Node2D
 
-func _input(event):
-	if Input.is_action_pressed("d_key"):
-		$NoteButtonD.frame = 1
-	
-	elif Input.is_action_pressed("f_key"):
-		$NoteButtonF.frame = 1
-		
-	elif Input.is_action_pressed("j_key"):
-		$NoteButtonJ.frame = 1
-		
-	elif Input.is_action_pressed("k_key"):
-		$NoteButtonK.frame = 1
-		
-	if Input.is_action_just_released("d_key"):
-		$NoteButtonD.frame = 0
-	
-	elif Input.is_action_just_released("f_key"):
-		$NoteButtonF.frame = 0
+var spawn_y # spawn offscreen
 
-	elif Input.is_action_just_released("j_key"):
-		$NoteButtonJ.frame = 0
-		
-	elif Input.is_action_just_released("k_key"):
-		$NoteButtonK.frame = 0
+var note_asset = load("res://scenes/Note.tscn")
+
--- game/scripts/NoteButton.gd
+++ game/scripts/NoteButton.gd
@@ -5,31 +5,40 @@ var good = false
 var great = false
 var current_note = null
 
-export var input = ""
+var is_pressed = false
+
+export(int, 1, 4) var lane
 
 
-func _unhandled_input(event):
-	if event.is_action(input):
-		if event.is_action_pressed(input, false):
-			if current_note != null:
-				if perfect:
-					get_parent().increment_score(100)
-					current_note.destroy(3)
-				elif great:
-					get_parent().increment_score(50)
-					current_note.destroy(2)
-				elif good:
-					get_parent().increment_score(25)
-					current_note.destroy(1)
-				_reset()
-			else:
-				get_parent().increment_score(0)
-		if event.is_action_pressed(input):
-			frame = 1
-		elif event.is_action_released(input):
-			$PushTimer.start()
-
-
+func _input(event):
+	match lane:
+		1:
+			if event.is_action_pressed("d_key"):
+				is_pressed = true
+			elif event.is_action_released("d_key"):
+				is_pressed = false
+		2:
+			if event.is_action_pressed("f_key"):
+				is_pressed = true
+			elif event.is_action_released("f_key"):
+				is_pressed = false
+		3:
+			if event.is_action_pressed("j_key"):
+				is_pressed = true
+			elif event.is_action_released("j_key"):
+				is_pressed = false
+		4:
+			if event.is_action_pressed("k_key"):
+				is_pressed = true
+			elif event.is_action_released("k_key"):
+				is_pressed = false
+	
+func _process(delta):
+	if is_pressed:
+		self.frame = 1
+	else:
+		self.frame = 0
+	
 func _on_PerfectArea_area_entered(area):
 	if area.is_in_group("note_group"):
 		perfect = true
@@ -63,7 +72,7 @@ func _on_GreatArea_area_exited(area):
 
 
 func _on_PushTimer_timeout():
-	frame = 0
+	pass
 
 
 func _reset():
